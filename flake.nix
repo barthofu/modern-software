1049d8d620c84647975a583be6d2c787a31b0ae3
Bartholomé Gili <dev.bartho@gmail.com>
2025-04-24 16:53:02 +0000

    init project

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.dotnetCorePackages.sdk_9_0
+            pkgs.dotnetCorePackages.runtime_9_0
+          ];
+
+          shellHook = ''
+            export DOTNET_CLI_TELEMETRY_OPTOUT=1
+            export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+            echo "✅ .NET dev environment ready"
+          '';
+        };
+      }
+    );
+}
